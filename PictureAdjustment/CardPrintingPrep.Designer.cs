6d2466463efa81c068ef67fa825152d5a2d5857b
Thai Le <tql9803@rit.edu>
2021-08-07 08:44:49 -0700

    Add project files.

---
+++ PictureAdjustment/CardPrintingPrep.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PictureAdjustment
+{
+    partial class CardPrintingPrep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tClientName = new System.Windows.Forms.TextBox();
+            this.lClientName = new System.Windows.Forms.Label();
+            this.bUpdate = new System.Windows.Forms.Button();
+            this.bSave = new System.Windows.Forms.Button();
+            this.pBMainCanvas = new System.Windows.Forms.PictureBox();
+            this.tClientNum = new System.Windows.Forms.TextBox();
+            this.bReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBMainCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tClientName
+            // 
+            this.tClientName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tClientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tClientName.Location = new System.Drawing.Point(92, 525);
+            this.tClientName.Name = "tClientName";
+            this.tClientName.Size = new System.Drawing.Size(410, 29);
+            this.tClientName.TabIndex = 0;
+            // 
+            // lClientName
+            // 
+            this.lClientName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lClientName.AutoSize = true;
+            this.lClientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lClientName.Location = new System.Drawing.Point(25, 530);
+            this.lClientName.Name = "lClientName";
+            this.lClientName.Size = new System.Drawing.Size(61, 24);
+            this.lClientName.TabIndex = 2;
+            this.lClientName.Text = "Name";
+            // 
+            // bUpdate
+            // 
+            this.bUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bUpdate.Location = new System.Drawing.Point(326, 494);
+            this.bUpdate.Name = "bUpdate";
+            this.bUpdate.Size = new System.Drawing.Size(147, 27);
+            this.bUpdate.TabIndex = 2;
+            this.bUpdate.Text = "Update Picture";
+            this.bUpdate.UseVisualStyleBackColor = true;
+            this.bUpdate.Click += new System.EventHandler(this.bUpdate_Click);
+            // 
+            // bSave
+            // 
+            this.bSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSave.Location = new System.Drawing.Point(798, 204);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(75, 60);
+            this.bSave.TabIndex = 3;
+            this.bSave.Text = "Save ";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // pBMainCanvas
+            // 
+            this.pBMainCanvas.Location = new System.Drawing.Point(29, 14);
+            this.pBMainCanvas.Name = "pBMainCanvas";
+            this.pBMainCanvas.Size = new System.Drawing.Size(750, 474);
+            this.pBMainCanvas.TabIndex = 5;
+            this.pBMainCanvas.TabStop = false;
+            // 
+            // tClientNum
+            // 
+            this.tClientNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tClientNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tClientNum.Location = new System.Drawing.Point(508, 525);
+            this.tClientNum.Name = "tClientNum";
+            this.tClientNum.Size = new System.Drawing.Size(271, 29);
+            this.tClientNum.TabIndex = 1;
+            // 
+            // bReset
+            // 
+            this.bReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bReset.Location = new System.Drawing.Point(798, 376);
+            this.bReset.Name = "bReset";
+            this.bReset.Size = new System.Drawing.Size(75, 60);
+            this.bReset.TabIndex = 3;
+            this.bReset.Text = "Reset";
+            this.bReset.UseVisualStyleBackColor = true;
+            this.bReset.Click += new System.EventHandler(this.bReset_Click);
+            // 
+            // CardPrintingPrep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(900, 560);
+            this.Controls.Add(this.pBMainCanvas);
+            this.Controls.Add(this.bReset);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.bUpdate);
+            this.Controls.Add(this.lClientName);
+            this.Controls.Add(this.tClientNum);
+            this.Controls.Add(this.tClientName);
+            this.Name = "CardPrintingPrep";
+            this.Text = "Card Imaging Preparation";
+            this.Load += new System.EventHandler(this.CardPrintingPrep_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBMainCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tClientName;
+        private System.Windows.Forms.Label lClientName;
+        private System.Windows.Forms.Button bUpdate;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.PictureBox pBMainCanvas;
+        private System.Windows.Forms.TextBox tClientNum;
+        private System.Windows.Forms.Button bReset;
+    }
+}
+
---
+++ PictureAdjustment/CardPrintingPrep.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureAdjustment
+{
+    public partial class CardPrintingPrep : Form
+    {
+        private Image OriginalImage;
+        private string ImageSavePath;
+
+        //FileStream fstream;
+        
+        public CardPrintingPrep()
+        {
+            InitializeComponent();
+        }
+
+        private void CardPrintingPrep_Load(object sender, EventArgs e)
+        {
+
+            //fstream = File.OpenRead("..\\..\\thu 7.jpg");
+            //OriginalImage = Image.FromStream(fstream, true);
+            //fstream.Close();
+
+
+            //Stream fstream;
+
+            //fstream = File.OpenRead("..\\..\\Backside.jpg");
+            //OriginalImage = Image.FromStream(fstream, true);
+            OriginalImage = global::PictureAdjustment.Properties.Resources.Backside;
+            //fstream.Close();
+
+            pBMainCanvas.Image = (Image)OriginalImage.Clone();
+            pBMainCanvas.SizeMode = PictureBoxSizeMode.StretchImage;
+            OriginalImage.Dispose();
+
+            //ft = new Font("Copperplate Gothic", (float)15.21, FontStyle.Bold, GraphicsUnit.Point);
+            //strFormat = new StringFormat();
+            //strFormat.Alignment = StringAlignment.Center;
+            //NameLoc = new Point(OriginalImage.Width / 2, OriginalImage.Height / 2);
+
+            //grp.DrawString("Random Name", ft, Brushes.CadetBlue, NameLoc, strFormat);
+
+            //pBMainCanvas.Image = OriginalImage;
+            //pBMainCanvas.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void UIChange(object sender, EventArgs e)
+        {
+            this.Size = this.Size + new Size(0, tClientName.Size.Height * 2);
+
+            tClientName.Location = new Point((this.Size.Width - tClientName.Size.Width) / 2, this.Height * 9 / 10);
+            lClientName.Location = new Point((this.Size.Width - tClientName.Size.Width) / 2 - lClientName.Size.Width, this.Height * 9 / 10);
+        }
+
+        private void ResetForm()
+        {
+            pBMainCanvas.Image = null;
+            tClientName.Text = null;
+            tClientNum.Text = null;
+
+            ImageSavePath = null;
+            OriginalImage = global::PictureAdjustment.Properties.Resources.Backside;
+
+            pBMainCanvas.Image = (Image)OriginalImage.Clone();
+            pBMainCanvas.SizeMode = PictureBoxSizeMode.StretchImage;
+            OriginalImage.Dispose();
+
+            GC.Collect();
+        }
+
+        private void DrawNameOnPic(string ClientName, string ClientNum)
+        {
+            //OriginalImage = global::PictureAdjustment.Properties.Resources.Backside;
+            //grp = Graphics.FromImage(pBMainCanvas.Image);
+            //grp.DrawString(ClientName, ft, Brushes.CadetBlue, NameLoc, strFormat);
+            Graphics grp;
+            Font ft, ft2;
+            StringFormat strFormat;
+            Point NameLoc, NumLoc;
+
+            Image bufim = pBMainCanvas.Image;
+            Bitmap bmp = new Bitmap(pBMainCanvas.Size.Width, pBMainCanvas.Size.Height);
+
+            //new Bitmap(OriginalImage.Width, OriginalImage.Height);
+
+            grp = Graphics.FromImage(bmp);
+
+            //pBMainCanvas.Image = (Image)OriginalImage.Clone();
+            //pBMainCanvas.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            ft = new Font(new FontFamily("Copperplate Gothic Bold"), 35f, FontStyle.Regular, GraphicsUnit.Point);
+            ft2 = new Font(new FontFamily("Copperplate Gothic Bold"), 20f, FontStyle.Regular, GraphicsUnit.Point);
+
+            strFormat = new StringFormat();
+            strFormat.Alignment = StringAlignment.Center;
+            strFormat.LineAlignment = StringAlignment.Center;
+
+            NameLoc = new Point(pBMainCanvas.Width / 2, pBMainCanvas.Height / 2 - 110);
+            NumLoc = new Point(pBMainCanvas.Width / 2, pBMainCanvas.Height / 2 - 65);
+
+            grp.DrawImage(bufim, 0,0, pBMainCanvas.Width, pBMainCanvas.Height);
+            grp.DrawString(ClientName.ToUpper(), ft, new SolidBrush(Color.DarkSlateBlue), NameLoc,strFormat);
+            grp.DrawString(ClientNum.ToUpper(), ft2, new SolidBrush(Color.DarkSlateBlue), NumLoc, strFormat);
+
+            pBMainCanvas.Image = bmp;
+
+            ft.Dispose();
+            ft2.Dispose();
+            grp.Dispose();
+            strFormat.Dispose();
+        }
+
+        private void SaveImage()
+        {
+            string newdir;
+            string FrontSavePath;
+            newdir = "C:\\MemIDDocument\\ClientsDocuments\\ClientPics\\" + tClientName.Text;
+            Directory.CreateDirectory(newdir);
+
+            ImageSavePath = newdir + "\\" + tClientName.Text + "B.jpg";
+            pBMainCanvas.Image.Save(ImageSavePath, ImageFormat.Jpeg);
+            FrontSavePath = newdir + "\\" + tClientName.Text + "F.jpg";
+
+            try
+            {
+                global::PictureAdjustment.Properties.Resources.Frontside.Save(FrontSavePath);
+            } catch(Exception e) 
+            { }
+        }
+
+        private void UpdateImage()
+        {
+            DrawNameOnPic(tClientName.Text,tClientNum.Text);
+        }
+        private void bUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateImage();
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+    }
+}
